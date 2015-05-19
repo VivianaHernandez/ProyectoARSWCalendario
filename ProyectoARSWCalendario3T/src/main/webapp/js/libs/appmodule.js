5b09bcd73de2fa1b71d52567469943634f244786
2085941 <2085941@linux39.labinfo.is.escuelaing.edu.co>
2015-05-19 15:38:01 -0500

    Asi sirve =)=)=)=)=)=

--- ProyectoARSWCalendario3T/src/main/webapp/js/libs/appmodule.js
+++ ProyectoARSWCalendario3T/src/main/webapp/js/libs/appmodule.js
@@ -25,12 +25,6 @@
                 $scope.arregloTareas = [];
                 $scope.arreglo = [];
                 
-                
-                
-      $scope.varia = "";
-            $scope.origen = "";
-            $scope.mensaje = "";
-                               
 //-----------------------------------------------------------------------------------------------------//                
 //--------------------------------------------Funciones para consultar---------------------------------//
 //-----------------------------------------------------------------------------------------------------//
@@ -103,66 +97,8 @@
                 
                  $scope.probar = function () {
                    alert('entro a probar'+$scope.declaracion);
-                };
-                
- 
-           
-  //-----------------------------------------------------------------------------------------------------//                
-//--------------------------------------------Funciones para websocket---------------------------------//
-//-----------------------------------------------------------------------------------------------------//
-          
-             $scope.pos =function(){
-                 alert('entro+++++++++++++++');
-                $http.post('rest/messages' , {"destiny": $scope.origen,"body":$scope.mensaje}).
-                        success(function(){
-                                      
-                }).error(function(){
-                    alert("NOOOOO");
-                });
-            };
-            
-              
-         $scope.insertAtCaret=function(areaId,text) {
-             
-    alert('entro+++++++++++++++');
-    var txtarea = document.getElementById(areaId);
-    var scrollPos = txtarea.scrollTop;
-    var strPos = 0;
-    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? 
-        "ff" : (document.selection ? "ie" : false ) );
-    if (br == "ie") { 
-        txtarea.focus();
-        var range = document.selection.createRange();
-        range.moveStart ('character', -txtarea.value.length);
-        strPos = range.text.length;
-    }
-    else if (br == "ff") strPos = txtarea.selectionStart;
+                };  
+            }
+    );
 
-    var front = (txtarea.value).substring(0,strPos);  
-    var back = (txtarea.value).substring(strPos,txtarea.value.length); 
-    txtarea.value=front+text+back;
-    strPos = strPos + text.length;
-    if (br == "ie") { 
-        txtarea.focus();
-        var range = document.selection.createRange();
-        range.moveStart ('character', -txtarea.value.length);
-        range.moveStart ('character', strPos);
-        range.moveEnd ('character', 0);
-        range.select();
-    }
-    else if (br == "ff") {
-        txtarea.selectionStart = strPos;
-        txtarea.selectionEnd = strPos;
-        txtarea.focus();
-    }
-    txtarea.scrollTop = scrollPos;
-}
-            });
-                  
-      
-})();
-
-
-
-
-
+})(); 
